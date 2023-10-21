9fbd0be7053d654158cde5ed258c364525686e46
Nikita Tyrkin <tyrkinn@pm.me>
2023-10-21 21:18:56 +0300

    feat(common): add role guard

---
+++ src/common/guards/role.guard.ts
@@ -0,0 +1,18 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { User } from '@prisma/client';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const roles = this.reflector.get<string[]>('roles', context.getHandler());
+    if (!roles) {
+      return true;
+    }
+    const request = context.switchToHttp().getRequest();
+    const user: User = request.user;
+    return roles.includes(user.role);
+  }
+}
