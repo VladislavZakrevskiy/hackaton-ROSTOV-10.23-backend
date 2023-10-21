f412bc567d806c5f5b32cdc020351013e67badd6
Nikita Tyrkin <tyrkinn@pm.me>
2023-10-22 01:17:24 +0300

    feat(common): add Authorized guard to Roles dec

--- src/common/decorators/roles.decorator.ts
+++ src/common/decorators/roles.decorator.ts
@@ -1,7 +1,11 @@
 import { SetMetadata, UseGuards, applyDecorators } from '@nestjs/common';
 import { Role } from '@prisma/client';
 import { RolesGuard } from '@/common/guards';
+import { Authorized } from '../guards/auth.guard';
 
 export function Roles(...roles: Role[]) {
-  return applyDecorators(SetMetadata('roles', roles), UseGuards(RolesGuard));
+  return applyDecorators(
+    SetMetadata('roles', roles),
+    UseGuards(RolesGuard, Authorized),
+  );
 }
