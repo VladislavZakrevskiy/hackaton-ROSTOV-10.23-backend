ba30de8d93f8d90a13f1b7d367eb388e1810e7c6
Nikita Tyrkin <tyrkinn@pm.me>
2023-10-21 23:42:01 +0300

    chore: update metadata

--- src/metadata.ts
+++ src/metadata.ts
@@ -1,5 +1,5 @@
 /* eslint-disable */
 export default async () => {
     const t = {};
-    return { "@nestjs/swagger/plugin": { "models": [], "controllers": [[import("./app.controller"), { "AppController": { "getUser": {} } }], [import("./users/users.controller"), { "UsersController": { "selectUsers": {} } }]] } };
+    return { "@nestjs/swagger/plugin": { "models": [[import("./events/dto/create-event.dto"), { "CreateEventDto": { specialist_id: { required: true, type: () => String } } }], [import("./events/dto/update-event.dto"), { "UpdateEventDto": {} }], [import("./events/dto/create-comment.dto"), { "CreateCommentDto": { text: { required: true, type: () => String, maxLength: 512 } } }]], "controllers": [[import("./app.controller"), { "AppController": { "getUser": {} } }], [import("./users/users.controller"), { "UsersController": { "selectUsers": {} } }], [import("./events/events.controller"), { "EventsController": { "getAll": {}, "getById": {}, "createEvent": {}, "update": {}, "addComment": {}, "recommendations": {} } }]] } };
 };
