2f2026acc98742b9591290df92352c090915afd0
Nikita Tyrkin <tyrkinn@pm.me>
2023-10-21 23:41:44 +0300

    feat(events): update events controller

--- src/events/events.service.ts
+++ src/events/events.service.ts
@@ -1,80 +1,27 @@
 import { PrismaService } from '@/common/services';
-import {
-  BadRequestException,
-  Injectable,
-  NotFoundException,
-} from '@nestjs/common';
-import { Event, Feedback, Specialist, EventStatus } from '@prisma/client';
+import { Injectable } from '@nestjs/common';
+import { Comment, Event, Specialist } from '@prisma/client';
 import { CreateEventDto } from './dto/create-event.dto';
-import { CreateFeedDto } from './dto/add-feedback.dto';
+import { UpdateEventDto } from './dto/update-event.dto';
+import { CreateCommentDto } from './dto/create-comment.dto';
 
 @Injectable()
 export class EventsService {
   constructor(private readonly prisma: PrismaService) {}
 
-  async getAll() {
-    const events = await this.prisma.event.findMany();
-    return events;
+  getAll(): Promise<Event[]> {
+    return this.prisma.event.findMany();
   }
 
-  async getById(id: string) {
-    try {
-      const event = await this.prisma.event.findFirst({
-        where: { id },
-        include: {
-          comments: true,
-          feedbacks: true,
-        },
-      });
-      return event;
-    } catch (error) {
-      throw new BadRequestException('Not valid id');
-    }
-  }
-
-  async groupEventsByStatusAndDays(): Promise<
-    { status: EventStatus; date: Date; events: Event[] }[]
-  > {
-    const events = await this.prisma.event.findMany();
-    const groupedEvents = events.reduce((result, event) => {
-      const status = event.status;
-      const startDate = event.start_date;
-
-      const existingGroup = result.find(
-        (group) =>
-          group.status === status && this.isSameDay(group.date, startDate),
-      );
-
-      if (existingGroup) {
-        existingGroup.events.push(event);
-      } else {
-        result.push({ status, date: startDate, events: [event] });
-      }
-
-      return result;
-    }, [] as { status: EventStatus; date: Date; events: Event[] }[]);
-
-    return groupedEvents;
-  }
-
-  async groupEventsByStatus(): Promise<
-    { status: EventStatus; events: Event[] }[]
-  > {
-    const events = await this.prisma.event.findMany();
-    const groupedEvents = events.reduce((result, event) => {
-      const status = event.status;
-      const existingGroup = result.find((group) => group.status === status);
-
-      if (existingGroup) {
-        existingGroup.events.push(event);
-      } else {
-        result.push({ status: event.status, events: [event] });
-      }
-
-      return result;
-    }, [] as { status: EventStatus; events: Event[] }[]);
-
-    return groupedEvents;
+  getById(id: string): Promise<Event> {
+    return this.prisma.event.findUnique({
+      where: { id },
+      include: {
+        comments: true,
+        feedbacks: true,
+        credentials: true,
+      },
+    });
   }
 
   private isSameDay(dateA: Date, dateB: Date): boolean {
@@ -85,153 +32,72 @@ export class EventsService {
     );
   }
 
-  async groupEventsByDays(): Promise<{ date: Date; events: Event[] }[]> {
-    const events = await this.prisma.event.findMany();
-    const groupedEvents = events.reduce((result, event) => {
-      const startDate = event.start_date;
-      const existingGroup = result.find((group) =>
-        this.isSameDay(group.date, startDate),
-      );
-
-      if (existingGroup) {
-        existingGroup.events.push(event);
-      } else {
-        result.push({ date: startDate, events: [event] });
-      }
-
-      return result;
-    }, [] as { date: Date; events: Event[] }[]);
-
-    return groupedEvents;
+  createEvent(event: CreateEventDto) {
+    const { specialist_id, ...eventData } = event;
+    return this.prisma.event.create({
+      data: {
+        ...eventData,
+        specialist: { connect: { id: specialist_id } },
+      },
+    });
   }
 
-  async createEvent(event: CreateEventDto) {
-    try {
-      const newEvent = await this.prisma.event.create({
-        data: {
-          description: event.description,
-          link: event.link,
-          name: event.name,
-          start_date: event.start_date,
-          status: event.status,
-          record_link: event.record_link,
-          specialist: {
-            create: {
-              first_name: event.specialist.first_name,
-              last_name: event.specialist.last_name,
-              social_link: event.specialist.social_link,
-            },
-          },
-        },
-      });
-      return newEvent;
-    } catch (error) {
-      throw new BadRequestException(error);
-    }
+  async update(event_id: string, updatedData: UpdateEventDto): Promise<Event> {
+    const updatedEvent = await this.prisma.event.update({
+      where: { id: event_id },
+      data: {
+        ...updatedData,
+      },
+    });
+    return updatedEvent;
   }
 
-  async update(event_id: string, updatedData: Partial<Event>): Promise<Event> {
-    try {
-      const updatedEvent = await this.prisma.event.update({
-        where: { id: event_id },
-        data: {
-          ...updatedData,
-          updated_at: new Date(),
-        },
-      });
-      return updatedEvent;
-    } catch (error) {
-      throw new BadRequestException(`Error update event id: ${event_id}`);
-    }
-  }
-
-  async addFeedBack(
+  addComment(
     eventId: string,
     userId: string,
-    feedback: CreateFeedDto,
-  ) {
-    const event = await this.prisma.event.findUnique({
-      where: { id: eventId },
-    });
-
-    if (!event) {
-      throw new NotFoundException('Событие не найдено');
-    }
-
-    const newFeedback = await this.prisma.feedback.create({
+    data: CreateCommentDto,
+  ): Promise<Comment> {
+    return this.prisma.comment.create({
       data: {
-        score: feedback.score,
-        text: feedback.text,
-        event: { connect: { id: eventId } },
-        user: { connect: { id: userId } },
-        specialist: { connect: { id: event.specialist_id } },
+        event_id: eventId,
+        user_id: userId,
+        ...data,
       },
     });
-
-    return newFeedback;
   }
 
-  async specialistsRec(): Promise<Specialist[]> {
-    const specialists = await this.prisma.specialist.findMany();
-
-    if (specialists.length === 0) {
-      return [];
-    }
-
-    const specialistPromises = specialists.map(
-      async (specialist: Specialist) => {
-        const feedbacks = await this.prisma.feedback.findMany({
-          where: {
-            specialist_id: specialist.id,
+  async specialistsRecomendations(): Promise<Specialist[]> {
+    const specialists = await this.prisma.specialist.findMany({
+      include: {
+        feedbacks: {
+          select: {
+            score: true,
           },
-        });
-
-        if (feedbacks.length > 0) {
-          const totalScore = feedbacks.reduce(
-            (sum, feedback) => sum + feedback.score,
-            0,
-          );
-          const averageScore = totalScore / feedbacks.length;
-
-          if (averageScore >= 4) {
-            return specialist;
-          }
-        }
+        },
       },
-    );
-
-    const specialistsAboveAverage = (await Promise.all(
-      specialistPromises,
-    )) as Specialist[];
-
-    return specialistsAboveAverage;
-  }
-
-  private calculateAverageScore(feedbacks: Feedback[]): number {
-    if (feedbacks.length === 0) {
-      return 0;
-    }
-
-    const totalScore = feedbacks.reduce(
-      (sum, feedback) => sum + feedback.score,
-      0,
-    );
-    return totalScore / feedbacks.length;
-  }
-
-  async getEventStatistics(): Promise<{ name: string; value: number }[]> {
-    const events = await this.prisma.event.findMany();
-
-    const statisticsPromises = events.map(async (event) => {
-      const feedbacks = await this.prisma.feedback.findMany({
-        where: { event_id: event.id },
-      });
-
-      const averageScore = this.calculateAverageScore(feedbacks);
-      return { name: event.name, value: averageScore };
     });
 
-    const statistics = await Promise.all(statisticsPromises);
-    return statistics;
-  }  
+    const sortedSpecialists = specialists
+      .map((s) => {
+        const { feedbacks, ...data } = s;
+        const totalScores = feedbacks.reduce((acc, v) => acc + v.score, 0);
+        return { ...data, scores: totalScores };
+      })
+      .sort((a, b) => a.scores - b.scores);
+
+    return sortedSpecialists.slice(0, 5);
+  }
+
+  async eventStatistics(eventId: string) {
+    const registredUsers = await this.prisma.user.findMany({
+      where: {
+        events: {
+          some: {
+            id: eventId,
+          },
+        },
+      },
+    });
+    return { registredUsersCount: registredUsers.length };
+  }
 }
