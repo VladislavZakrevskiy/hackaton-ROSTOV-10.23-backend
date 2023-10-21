161b3435aa15d07e427e03853e60847e0e683bbe
Nikita Tyrkin <tyrkinn@pm.me>
2023-10-21 23:41:26 +0300

    fix(events): change events controller

--- src/events/events.controller.ts
+++ src/events/events.controller.ts
@@ -1,64 +1,45 @@
 import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
 import { EventsService } from './events.service';
-import { Event, Specialist } from '@prisma/client';
+import { Event, Specialist, User } from '@prisma/client';
 import { CreateEventDto } from './dto/create-event.dto';
-import { CreateFeedDto } from './dto/add-feedback.dto';
+import { GUser } from '@/common/decorators/user.decorator';
+import { CreateCommentDto } from './dto/create-comment.dto';
 
 @Controller('event')
 export class EventsController {
   constructor(private readonly eventService: EventsService) {}
 
   @Get('/all')
-  async getAll(): Promise<Event[]> {
-    return await this.eventService.getAll();
-  }
-
-  @Get('/stats')
-  async getStats() {
-    return await this.eventService.getEventStatistics();
-  }
-
-  @Get('/group-by-days')
-  async groupByDay() {
-    return await this.eventService.groupEventsByDays();
-  }
-
-  @Get('/group-by-status')
-  async groupByStatus() {
-    return await this.eventService.groupEventsByStatus();
-  }
-
-  @Get('/group-by-day-status')
-  async groupByStatusAndDays() {
-    return await this.eventService.groupEventsByStatusAndDays();
+  getAll(): Promise<Event[]> {
+    return this.eventService.getAll();
   }
 
   @Get(':id')
-  async getById(@Param('id') id: string): Promise<Event> {
-    return await this.eventService.getById(id);
+  getById(@Param('id') id: string): Promise<Event> {
+    return this.eventService.getById(id);
   }
 
   @Post()
-  async createEvent(body: CreateEventDto) {
-    return await this.eventService.createEvent(body);
+  createEvent(body: CreateEventDto) {
+    return this.eventService.createEvent(body);
   }
 
   @Put(':id')
-  async update(@Param('id') id: string, data: Partial<Event>) {
-    return await this.eventService.update(id, data);
+  update(@Param('id') id: string, data: Partial<Event>) {
+    return this.eventService.update(id, data);
   }
 
-  @Post(':id/feedback')
-  async addFeedback(
+  @Post(':id/comment')
+  addComment(
     @Param('id') id: string,
-    userId: string,
-    @Body() body: CreateFeedDto,
+    @GUser() user: User,
+    @Body() body: CreateCommentDto,
   ) {
-    return await this.eventService.addFeedBack(id, userId, body);
+    return this.eventService.addComment(id, user.id, body);
   }
 
   @Get('recommendations')
-  async recommendations(): Promise<Specialist[]> {
-    return await this.eventService.specialistsRec();
+  recommendations(): Promise<Specialist[]> {
+    return this.eventService.specialistsRecomendations();
   }
 }
