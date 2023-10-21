f8d689c7e696f5005d21a443a2938ddc729ea582
Nikita Tyrkin <tyrkinn@pm.me>
2023-10-22 02:17:54 +0300

    feat(events): update create-event dto

--- src/events/dto/create-event.dto.ts
+++ src/events/dto/create-event.dto.ts
@@ -1,10 +1,25 @@
 import { ClearEventModel } from '@/models/clear-event.model';
+import { CredentialsModel } from '@/models/credentials.model';
 import { ApiProperty, OmitType } from '@nestjs/swagger';
-import { IsNotEmpty, IsString } from 'class-validator';
+import { Type } from 'class-transformer';
+import {
+  IsArray,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  ValidateNested,
+} from 'class-validator';
 
 export class CreateEventDto extends OmitType(ClearEventModel, ['id']) {
   @ApiProperty()
   @IsString()
   @IsNotEmpty()
   specialist_id: string;
+
+  @IsArray()
+  @IsOptional()
+  @ValidateNested({ each: true })
+  @ApiProperty({ required: false, isArray: true, type: CredentialsModel })
+  @Type(() => CredentialsModel)
+  credentials: CredentialsModel[];
 }
