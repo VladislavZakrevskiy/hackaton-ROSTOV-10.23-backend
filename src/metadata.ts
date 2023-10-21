11cd30f5231a8be699e6b9b847cf7b5a24bbdaf5
Nikita Tyrkin <tyrkinn@pm.me>
2023-10-22 00:54:38 +0300

    chore: update metadata

--- src/metadata.ts
+++ src/metadata.ts
@@ -1,5 +1,8 @@
 /* eslint-disable */
 export default async () => {
-    const t = {};
-    return { "@nestjs/swagger/plugin": { "models": [[import("./events/dto/create-event.dto"), { "CreateEventDto": { specialist_id: { required: true, type: () => String } } }], [import("./events/dto/update-event.dto"), { "UpdateEventDto": {} }], [import("./events/dto/create-comment.dto"), { "CreateCommentDto": { text: { required: true, type: () => String, maxLength: 512 } } }]], "controllers": [[import("./app.controller"), { "AppController": { "getUser": {} } }], [import("./users/users.controller"), { "UsersController": { "selectUsers": {} } }], [import("./events/events.controller"), { "EventsController": { "getAll": {}, "getById": {}, "createEvent": {}, "update": {}, "addComment": {}, "recommendations": {} } }]] } };
+    const t = {
+        ["./models/clear-event.model"]: await import("./models/clear-event.model"),
+        ["./models/comment.model"]: await import("./models/comment.model")
+    };
+    return { "@nestjs/swagger/plugin": { "models": [[import("./events/dto/create-event.dto"), { "CreateEventDto": { specialist_id: { required: true, type: () => String } } }], [import("./events/dto/update-event.dto"), { "UpdateEventDto": {} }], [import("./events/dto/create-comment.dto"), { "CreateCommentDto": { text: { required: true, type: () => String, maxLength: 512 } } }]], "controllers": [[import("./app.controller"), { "AppController": { "getUser": {} } }], [import("./users/users.controller"), { "UsersController": { "selectUsers": {} } }], [import("./events/events.controller"), { "EventsController": { "getAll": { type: [t["./models/clear-event.model"].ClearEventModel] }, "getById": {}, "createEvent": { type: t["./models/clear-event.model"].ClearEventModel }, "update": { type: t["./models/clear-event.model"].ClearEventModel }, "addComment": { type: t["./models/comment.model"].CommentModel }, "recommendations": {} } }]] } };
 };
